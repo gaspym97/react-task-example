c8e548b6275606d6b2dfbd9ce64b5265c5147fff
gaspym97 <angelgasparmiralles@gmail.com>
2024-02-24 16:58:34 -0500

    first commit

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,21 @@
+import TaskCard from "./TaskCard";
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
+
+function TaskList() {
+  const { tasks } = useContext(TaskContext);
+
+  if (tasks.length === 0) {
+    return <div className="text-white font-bold text-4xl">Empty</div>;
+  }
+
+  return (
+    <div className="grid grid-cols-4 gap-2">
+      {tasks.map((task) => (
+        <TaskCard key={task.id} task={task} />
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;
---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "my first task",
+    description: "this is the first task",
+  },
+  {
+    id: 1,
+    title: "my second task",
+    description: "this is the second task",
+  },
+  {
+    id: 2,
+    title: "my third task",
+    description: "this is the third task",
+  },
+];
